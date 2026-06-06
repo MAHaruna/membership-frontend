339a453baf6fc94ada48f497ace950e48cbb49e6
MAHaruna <musaauwalharuna@gmail.com>
2026-06-06 21:19:27 +0300

    locationservice

--- src/services/locationService.js
+++ src/services/locationService.js
@@ -1,5 +1,27 @@
 import api from "../api/axios";
 
-export const getStates = () => api.get("states/").then(r => r.data);
-export const getLGAs = () => api.get("local-governments/").then(r => r.data);
-export const getWards = () => api.get("wards/").then(r => r.data);
+export const getStates = async () => {
+  const res = await api.get("states/");
+  return res.data;
+};
+
+export const getZones = async () => {
+  const res = await api.get("zones/");
+  return res.data;
+};
+
+export const getLGAs = async () => {
+  const res = await api.get("lgas/");
+  return res.data;
+};
+
+export const getWards = async () => {
+  const res = await api.get("wards/");
+  return res.data;
+};
+
+// ✅ THIS IS THE ONE YOU ARE MISSING
+export const getPollingUnits = async () => {
+  const res = await api.get("polling-units/");
+  return res.data;
+};
